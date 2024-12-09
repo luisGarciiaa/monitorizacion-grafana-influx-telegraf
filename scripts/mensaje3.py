4cd1958ed5dc4794cad9367dafb22a4f6bf60a21
luisGarciiaa <106177134+luisGarciiaa@users.noreply.github.com>
2024-12-09 17:16:13 +0100

    Update mensaje3.py

--- scripts/mensaje3.py
+++ scripts/mensaje3.py
@@ -1,6 +1,5 @@
 import requests
 
-#url = "https://httpstat.us/200?sleep=5000"
 url = "http://localhost:27017/"
 alias="mongodb"
 keywords = "backend\,webserver\,real"
@@ -20,26 +19,3 @@ except requests.exceptions.RequestException as e:
     error_message = str(e).replace('"', '\\"')  # Escapa comillas en el mensaje
     print(f"http_response,url={url},source=url,nombre=\"{alias}\",keywords=\"{keywords}\" status_code=500,message=\"{error_message}\"")
 
-#CODIGO DE ABAJO PARA PRUEBAS CON TIPOS DE ERROR 
-#DIFERENTES DEPENDIENDO DE LA URL
-
-#url = "https://httpstat.us/404"  # URL que genera un error 404 con un mensaje en HTML
-#url= "https://httpstat.us/403"
-#url = "https://httpstat.us/500"
-#url = "https://httpstat.us/200?sleep=5000"
-'''
-try:
-    response = requests.get(url, timeout=1)  # Timeout de 3 segundos
-    
-    if response.status_code == 200:
-        print("OK")
-    else:
-        # Si el código de estado no es 200, imprime el código y el contenido de la respuesta
-        #print(f"Error: Código de estado {response.status_code}")
-        print("Mensaje de error:", response.text)  # Imprime el contenido del mensaje de error
-
-except requests.exceptions.Timeout:
-    print("Timeout")
-except requests.exceptions.RequestException as e:
-    print(f"Error en la solicitud: {e}")
-'''
